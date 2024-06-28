06b871b1ba9ecdc0006f1eb0f60f6a5d337b701a
Godfrey Obiero <godfrey.osun@gmail.com>
2024-06-28 10:01:56 -0700

    io.js contains function for accessing json files on your local computer

--- io.js
+++ io.js
@@ -1,24 +1,32 @@
-document.getElementById('input_JSON_file').addEventListener('change', function(event) {
-    const file = event.target.files[0]
-    if(file){
-        const reader = new FileReader();
-        reader.onload = function(e) {
-            try {
-                const JSON_data = JSON.parse(e.target.result);
-                document.getElementById('parse_JSON').textContent = JSON.stringify(JSON_data, null, 2);
-            }catch(error){
-                console.error('failed to parse json file', error);
-                document.getElementById('parse_JSON').textContent = 'failed to parse json file: error' + error.message;
+useExternalJSON()
+
+function useExternalJSON(){
+    const JSON_data_struct = Null;
+    document.getElementById('input_JSON_file').addEventListener('change', function(event) {
+        const file = event.target.files[0]
+        if(file){
+            const reader = new FileReader();
+            reader.onload = function(e) {
+                try {
+                    const JSON_data = JSON.parse(e.target.result);
+                    JSON_data_struct = JSON.stringify(JSON_data, null, 2);
+                    document.getElementById('parse_JSON').textContent = JSON_data_struct;
+                }catch(error){
+                    console.error('failed to parse json file', error);
+                    document.getElementById('parse_JSON').textContent = 'failed to parse json file: error' + error.message;
+                    }
                 }
+                reader.readAsText(file)
+            }else{
+                console.error('no file was loader!');
+                console.error('must use default json!');
+                useDefaultJSON();
             }
-            reader.readAsText(file)
-        }else{
-            console.error('no file was loader!');
-            console.error('must use default json!');
-            useDefaultJSON();
         }
-    }
-)
+    )
+    return JSON_data_struct;
+}
+
 
 // use the default json file for debugging
 function useDefaultJSON(){
