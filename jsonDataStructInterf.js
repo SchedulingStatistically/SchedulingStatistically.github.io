52f3c636ca035c747d609eb65d2201a64ff861b9
Godfrey Obiero <godfrey.osun@gmail.com>
2024-06-30 15:32:34 -0700

    submission 5 modified files, jsonDataStuctInterf.html is used for debugging jsonDataStructInter with web browsar and jsonDataStructInter.js contain function which the frontend devs use to updata data struct, new file jsonDataStructInterfDebugTest.js are used to debug jsonDataStructInterf.js

--- jsonDataStructInterf.js
+++ jsonDataStructInterf.js
@@ -1,1 +1,116 @@
+/*
+    DO NOT MODIFY THE MAIN BRANCH OF jsonDataStructInterf.js unless YOUR MERGE IS APPROVED
+
+    to test and modify the code, create your BRANCH
+    before you merge your branch, the backend team must review YOUR BRANCH for APPROVAL!
+
+*/
+
+import  {ScheduledEvents, OwnerStatus, Ownership, JsonDataStruct} from './jsonDataStruct.js'
+// loading a whole json to make a data struct
+const whole_json = {
+    name : 'whole tree',
+    ownership : {
+        name : 'sam',
+        user_name : 'to',
+        password : 'sam to'
+    },
+    owner_status : {
+        max : 17,
+        min : 0,
+        mode : 3,
+        median : 13
+    },
+    scheduled_events : [
+        {
+            year : 2019,
+            month : 9,
+            day : 9,
+            hobby : 'coding',
+            start : '9:00am',
+            hours : 4,
+            end : '1:00pm'
+        },
+        {
+            year : 2020,
+            month : 7,
+            day : 1,
+            hobby : 'typing',
+            start : '11:00am',
+            hours : 1,
+            end : '1:00pm'
+        },
+        {
+            year : 2020,
+            month : 8,
+            day : 2,
+            hobby : 'reading',
+            start : '12:00am',
+            hours : 1,
+            end : '1:00pm'
+        },
+        {
+            year : 2020,
+            month : 8,
+            day : 2,
+            hobby : 'reading',
+            start : '4:00apm',
+            hours : 1,
+            end : '5:00pm'
+        }
+    ]
+}
 // pending documentation!
+export default class jsonDataStructInterf {
+    constructor(name){
+        this.name = name;
+        this.json_object = null;
+    }
+
+    use_default_json_data(){
+        this.json_object = JsonDataStruct.fromJsonFormat(whole_json);
+    }
+
+    updateOwnership(name, user_name, password){
+        let owner = {
+            name : name,
+            user_name : user_name,
+            password : password
+        }
+        this.json_object.updateOwnership(owner);
+    }
+
+    updateOwnerStatus(max, min, median, mode){
+        let status = {
+            max : max,
+            min : min,
+            median : median,
+            mode : mode
+        }
+        this.json_object.updateOwner_status(status)
+    }
+
+    addComplete_events(year, month, day, hobby, start, hours, end){
+        let a_event = {
+            year : year,
+            month : month,
+            day : day,
+            hobby : hobby,
+            start : start,
+            hours : hours,
+            end : end
+        }
+        this.json_object.addScheduled_event(a_event)
+    }
+
+    removeCompleted_event(){
+        this.json_object.removeScheduled_event()
+    }
+
+    getJsonObjectString(){
+        const json_string = JSON.stringify(this.json_object.toJsonFormat(), null, 2);
+        return json_string;
+    }
+
+
+}
---
+++ jsonDataStructInterfDebugTest.js
@@ -0,0 +1,47 @@
+/*
+
+    USE FOR DEBUGGING
+
+    HOW TO TEST THE CODE
+
+    "Use and the jsonDataStructInterf.js to run and test the JsonDataStructInterf object"
+    "run JsonDataStructInterf.html on a web browser get the console.log output with inspect command"
+    "I use vscode with 'Live Server' extension to run the html on my local browser app"
+    "DO NOT MERGE or push you testing code in the main branch"
+
+    UNLESS the change you have made in jsonDataStructInter.js has been approve
+    show how the changes you have made works below DOCUMENT THE CHANGES PROPERLY.
+
+*/
+
+import jsonDataStructInterf from "./jsonDataStructInterf.js";
+
+console.log('debugging jsonDataStructInterf');
+const our_interface = new jsonDataStructInterf('face');
+our_interface.use_default_json_data()
+console.log('print initialize interface object')
+console.log(our_interface)
+
+// update ownership on interface
+console.log('update ownership on interface')
+our_interface.updateOwnership('jen', 'will', 'jen will')
+console.log(our_interface)
+
+// update owner status on interface
+console.log('update owner status on interface')
+our_interface.updateOwnerStatus(69, 13, 33, 21)
+console.log(our_interface)
+
+// add a scheduled event
+console.log('add a scheduled event')
+our_interface.addComplete_events(2022, 10, 3, 'web development', '1:00pm', 3, '4:00pm')
+console.log(our_interface)
+
+// remove a schedule event in lilo queue
+console.log('remove a schedule event in lilo queue')
+our_interface.removeCompleted_event()
+console.log(our_interface)
+
+// get json object string
+console.log('get json object string')
+console.log(our_interface.getJsonObjectString());
